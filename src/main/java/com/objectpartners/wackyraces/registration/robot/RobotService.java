fe2a8bd5b819ca485c22b2c8710768c04783275b
jacobaseverson <jacoba.severson@gmail.com>
2015-01-04 17:20:03 -0600

    added stubbed robot service and test and updated the robot controller to use it

---
+++ src/main/java/com/objectpartners/wackyraces/registration/robot/RobotService.java
@@ -0,0 +1,11 @@
+package com.objectpartners.wackyraces.registration.robot;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RobotService {
+
+    public String registerRobot(Robot robot) {
+        return "testRobotId";
+    }
+}
--- src/main/java/com/objectpartners/wackyraces/registration/robot/web/RobotController.java
+++ src/main/java/com/objectpartners/wackyraces/registration/robot/web/RobotController.java
@@ -1,5 +1,8 @@
 package com.objectpartners.wackyraces.registration.robot.web;
 
+import com.objectpartners.wackyraces.registration.robot.Robot;
+import com.objectpartners.wackyraces.registration.robot.RobotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RobotController {
 
+    @Autowired
+    RobotService robotService;
+
     @RequestMapping(value = "/register/robot", method = RequestMethod.POST)
     public RegistrationResponse registerRobot(@RequestBody RegistrationRequest request) {
-        return new RegistrationResponse("testrobotid");
+        //TODO: build robot from request
+        return new RegistrationResponse(robotService.registerRobot(new Robot()));
     }
 
 }
