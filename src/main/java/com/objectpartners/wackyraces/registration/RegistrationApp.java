70f3c7a0824ba943b0acd6385ae83598bf31c61f
jacobaseverson <jacoba.severson@gmail.com>
2015-01-04 18:15:40 -0600

    swapped out spring data rest for spring web and added component scan so the controller gets wired

--- src/main/java/com/objectpartners/wackyraces/registration/RegistrationApp.java
+++ src/main/java/com/objectpartners/wackyraces/registration/RegistrationApp.java
@@ -2,10 +2,12 @@ package com.objectpartners.wackyraces.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@ComponentScan
 @EnableMongoRepositories
 @EnableAutoConfiguration
 public class RegistrationApp {
