b0c839ac1be054754bed9cad4f50ba879897573b
jacobaseverson <jacoba.severson@gmail.com>
2015-01-10 16:17:39 -0600

    Added properties file and property config for the db name

--- src/main/java/com/objectpartners/wackyraces/registration/config/MongoConfig.java
+++ src/main/java/com/objectpartners/wackyraces/registration/config/MongoConfig.java
@@ -1,6 +1,7 @@
 package com.objectpartners.wackyraces.registration.config;
 
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
@@ -10,9 +11,12 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 @Configuration
 public class MongoConfig {
 
+    @Value("${com.objectpartners.dbName}")
+    private String dbName;
+
     @Bean
     public MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory(new MongoClient(), "registration");
+        return new SimpleMongoDbFactory(new MongoClient(), dbName);
     }
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
